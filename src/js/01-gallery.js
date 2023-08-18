2b5f093136b7d4c7f34cdc1a6636ad440ef98245
NadiyaShrai <134630278+deagorn@users.noreply.github.com>
2023-08-18 15:42:11 +0300

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+
+const markup = galleryItems
+    .map((items) => `<li class="gallery__item">
+   <a class="gallery__link" href="${items.original}">
+      <img class="gallery__image" src="${items.preview}" alt="${items.description}" />
+   </a>
+</li>`)
+    .join("");
+
+
+
+
+// Adding a gallery to HTML
+const gallery = document.querySelector(".gallery");
+gallery.innerHTML = markup;
+
+
+ // creating a modal window
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt',
+     captionDelay: 250  });
+
